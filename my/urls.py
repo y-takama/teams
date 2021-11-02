78ddb290a225ba34e6420012750f350509566462
TAKAMA <caption.service@outlook.jp>
2021-11-02 20:42:43 +0900

    11/2

--- my/urls.py
+++ my/urls.py
@@ -8,7 +8,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('projects.urls'))
+    path('projects/', include('projects.urls')),
+    path('', include('users.urls')),
     #path('home/', admin.site.urls),
     #path('projects/', projects, name='projects'),
     #path('profiles/', admin.site.urls),
